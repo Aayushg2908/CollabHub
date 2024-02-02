78de95e415bb626151c0c07a400663270e3ec418
Aayush <aayushg2908@gmail.com>
2024-02-02 21:00:09 +0530

    feat: adding realtime cursors in whiteboard room

--- app/(main)/whiteboard/[roomId]/_components/Room.tsx
+++ app/(main)/whiteboard/[roomId]/_components/Room.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import Cursor from "@/components/Cursor";
 import { Loading } from "@/components/Loading";
-import { RoomProvider, useOthersMapped, useSelf } from "@/liveblocks.config";
+import {
+  RoomProvider,
+  useOthersMapped,
+  useSelf,
+  useUpdateMyPresence,
+} from "@/liveblocks.config";
 import { Layer } from "@/types";
 import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
 import { ClientSideSuspense } from "@liveblocks/react";
 import { Avatar, AvatarGroup, Snippet, Tooltip } from "@nextui-org/react";
+
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
 export const Room = ({ roomId }: { roomId: string }) => {
   return (
@@ -36,9 +44,26 @@ const Canvas = ({ roomId }: { roomId: string }) => {
     cursor: other.presence.cursor,
     info: other.info,
   }));
+  const updateMyPresence = useUpdateMyPresence();
 
   return (
-    <main className="h-full w-full relative touch-none">
+    <main
+      onPointerMove={(event) => {
+        const newCursor = {
+          x: event.clientX,
+          y: event.clientY,
+        };
+        updateMyPresence({
+          cursor: newCursor,
+        });
+      }}
+      onPointerLeave={() =>
+        updateMyPresence({
+          cursor: null,
+        })
+      }
+      className="h-screen w-full relative touch-none flex flex-col overflow-hidden"
+    >
       <div className="w-full absolute top-0 flex justify-between items-center">
         <Snippet symbol="">http://localhost:3000/whiteboard/{roomId}</Snippet>
         <AvatarGroup className="bg-neutral-800 py-2 px-6 rounded-md w-fit">
@@ -60,6 +85,21 @@ const Canvas = ({ roomId }: { roomId: string }) => {
           )}
         </AvatarGroup>
       </div>
+
+      {others.map(([id, other]) => {
+        if (other.cursor == null) {
+          return null;
+        }
+        return (
+          <Cursor
+            key={id}
+            color={COLORS[id % COLORS.length]}
+            x={other.cursor.x}
+            y={other.cursor.y}
+            name={other.info?.name}
+          />
+        );
+      })}
     </main>
   );
 };
